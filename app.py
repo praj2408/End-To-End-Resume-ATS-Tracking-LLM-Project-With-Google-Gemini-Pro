54802bbd0ad231520b1d49de0c23833ca5251ad0
Prajwal Krishna <prajwalgbdr03@gmail.com>
2024-01-26 03:07:37 -1200

    fix1

--- app.py
+++ app.py
@@ -1,61 +1,63 @@
 import streamlit as st
 import google.generativeai as genai
-
 import os
-import PyPDF2 as pdf 
-
+import PyPDF2 as pdf
 from dotenv import load_dotenv
+import json
 
-load_dotenv() ## laod all the evironment variables
+load_dotenv() ## load all our environment variables
 
-genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-
-## GEmini Pro Response
-def get_gemini_response(input):
-    
-    model=genai.GenerativeModel("gemini-pro")
+def get_gemini_repsonse(input):
+    model=genai.GenerativeModel('gemini-pro')
     response=model.generate_content(input)
-    
     return response.text
-
 
 def input_pdf_text(uploaded_file):
     reader=pdf.PdfReader(uploaded_file)
     text=""
-    for page in reader(len(reader.pages)):
+    for page in range(len(reader.pages)):
         page=reader.pages[page]
         text+=str(page.extract_text())
-        
     return text
 
+#Prompt Template
 
-## prompt Template
-
-input_prompt= """
-Hey Act like a skiled or very experience ATS(Application Tracking System) with a deep understanding of tech field, software engineering, data science, data analyst and big data engineer. Your task is
-to evaluate the resume based on the given job description. You must considerr the job marker is very competitive and you should provide best assistance for improving the resumes.
-Assign the percentage matching based on Jd and the missing keywords with high accuracy
+input_prompt="""
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
+with a deep understanding of tech field,software engineering,data science ,data analyst
+and big data engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide 
+best assistance for improving thr resumes. Assign the percentage Matching based 
+on Jd and
+the missing keywords with high accuracy
 resume:{text}
 description:{jd}
-I want the response in one single string having the structure
-{{"JD match": "%", "MissingKeywords: []", "Profile Summary: ""}}
+
+I want the response as per below structure
+{{"JD Match": "%", "MissingKeywords": [], "Profile Summary": ""}}
 """
 
-
-
 ## streamlit app
-st.title("Smart Applications Tracking System(ATS)")
+
+with st.sidebar:
+    st.title("Smart ATS")
+    st.write("This sophisticated ATS project, developed with Gemini Pro and Streamlit, seamlessly incorporates advanced features including resume match percentage, keyword analysis to identify missing criteria, and the generation of comprehensive profile summaries, enhancing the efficiency and precision of the candidate evaluation process for discerning talent acquisition professionals.")
+    
+    st.
+    
+
+
+st.title("Smart Application Tracking System")
 st.text("Improve Your Resume ATS")
-jd=st.text_area("Paste your Job Description")
-uploaded_file=st.file_uploader("Upload your Resume", type="pdf", help="Please upload your resume")
+jd=st.text_area("Paste the Job Description")
+uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
 
 submit = st.button("Submit")
-
 
 if submit:
     if uploaded_file is not None:
         text=input_pdf_text(uploaded_file)
-        response = get_gemini_response(input_prompt)
+        response=get_gemini_repsonse(input_prompt)
         st.subheader(response)
-        
